581d84d25932f4c221e8a232ef2c34b1feb875a5
Rachel Hamby <tnthreat33@gmail.com>
2023-01-23 19:00:04 -0500

    commit tree 270e5262a9a3328951d99e73f995a6595cf0c8b1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+  
+    let ul = document.querySelector("#list")
+    let div = document.querySelector("#show-panel")
+
+    fetch("http://localhost:3000/books")
+    .then(res => res.json())
+    .then(books=>{
+        books.forEach(book => {
+            let li = document.createElement("li")
+            li.innerText = book.title
+            ul.appendChild(li)
+            li.addEventListener("click", ()=>{
+                let p = document.createElement("p")
+                div.append(p)
+                p.innerText = `Book Title: ${book.title}
+               Book Description: ${book.description}
+                Users Who Like This Book: ${book.users[0].username}`
+                let img = document.createElement("img")
+                div.append(img)
+                img.src = book.img_url
+            })
+        })
+    }
+    
+        )
+}
+
+
+
+);
